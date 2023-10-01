72ca757a61645127f42359e13fbf04b6b25d6035
gujarathijugal <139682461+gujarathijugal@users.noreply.github.com>
2023-10-02 02:01:40 +0530

    Update stackandheap.js

--- basics/stackandheap.js
+++ basics/stackandheap.js
@@ -9,8 +9,10 @@ lis={
 lis2=lis
 ans if lis2.email="abc@pmail.com"
 then lis.email is also equal to "abc@pmail.com"
-non prinitve datatypes are stored in heap and we get reference of this values in stack so if we assign the same non primitive value to another variable and change it for another variable then it also changes for original variable
+non primitve datatypes are stored in heap and we get reference of this values in stack so if we assign the same
+non primitive value to another variable and change it for another variable then it also changes for original variable
 
-heap->non primitive->ives reference ->changes other variable even if it is accessed by other variable as both have same reference to same pointer
+heap->non primitive->ives reference ->changes other variable even if it is accessed by other variable as 
+both have same reference to same pointer
 stack->primitive->gives copy->does not change original if accessed through other variable
 */
