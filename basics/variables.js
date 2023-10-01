a733695b62f7deecf38f4b7c4237a91142f6b38b
gujarathijugal <139682461+gujarathijugal@users.noreply.github.com>
2023-10-01 11:14:10 +0000

    variables

---
+++ basics/variables.js
@@ -0,0 +1,12 @@
+const accountId=252525;
+let accountName="Tanjiro";
+var accountEmail="Tanjiro@gnail.com";
+accountadd="swordsmith village";
+let accountState;
+// accountId=15144; //not possiblee to change const
+console.table([accountId,accountName,accountEmail,accountadd,accountState])
+accountName="Rengoku";
+accountEmail="Rengoku@gmail.com"
+accountadd="entertainment district"
+
+console.table([accountId,accountName,accountEmail,accountadd,accountState])
